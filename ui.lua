8832083246264b7195054a4e09f56e973efcde43
Benjamin Rabier <rabier.benj@gmail.com>
2023-03-23 18:58:53 +0100

    fix nvimt-tree issue with auto session

--- ui.lua
+++ ui.lua
@@ -56,6 +56,19 @@ vim.cmd([[
   noremap <silent><C-n> <cmd>NvimTreeToggle<cr>
   noremap <silent><leader>tf <cmd>NvimTreeFocus<cr>
 ]])
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#workaround-when-using-rmagattiauto-session
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = 'NvimTree*',
+  callback = function()
+    local view = require('nvim-tree.view')
+    local is_visible = view.is_visible()
+
+    local api = require('nvim-tree.api')
+    if not is_visible then
+      api.tree.open()
+    end
+  end,
+})
 
 require('trouble').setup({
   padding = false,
@@ -81,8 +94,8 @@ telescope.setup({
     -- path_display = { 'smart' },
     mappings = {
       i = {
-            ["<C-t>"] = trouble.open_with_trouble,
-            ["<esc>"] = require('telescope.actions').close
+        ["<C-t>"] = trouble.open_with_trouble,
+        ["<esc>"] = require('telescope.actions').close
       },
       n = { ["<C-t>"] = trouble.open_with_trouble },
     },
