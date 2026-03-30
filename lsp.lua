9e8172cf21550b7321fade346189dfbfb1a229f5
Benjamin Rabier <benjamin+git@rabier.dev>
2026-03-30 15:23:32 +0200

    zls

--- lsp.lua
+++ lsp.lua
@@ -209,16 +209,16 @@ vim.lsp.config['zls'] = {
   -- Further information on how to configure ZLS:
   -- https://zigtools.org/zls/configure/
   settings = {
-    zls = {
-      -- Whether to enable build-on-save diagnostics
-      --
-      -- Further information about build-on save:
-      -- https://zigtools.org/zls/guides/build-on-save/
-      -- enable_build_on_save = true,
-
-      -- omit the following line if `zig` is in your PATH
-      -- zig_exe_path = '/path/to/zig_executable'
-    }
+    -- zls = {
+    --   -- Whether to enable build-on-save diagnostics
+    --   --
+    --   -- Further information about build-on save:
+    --   -- https://zigtools.org/zls/guides/build-on-save/
+    --   -- enable_build_on_save = true,
+    --
+    --   -- omit the following line if `zig` is in your PATH
+    --   -- zig_exe_path = '/path/to/zig_executable'
+    -- }
   },
 }
 vim.lsp.enable('zls')
